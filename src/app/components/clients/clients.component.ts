a2a29a79cb25b0a7c36f325100c93dd83e46a463
chris steigerwald <cjsteigerwald@gmail.com>
2020-07-06 09:02:25 -0700

    Adding Dashboard that shows details of clients, and total balance owed

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -5,17 +5,24 @@ import { Client } from 'src/app/models/Client';
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.css']
+  styleUrls: ['./clients.component.css'],
 })
 export class ClientsComponent implements OnInit {
   clients: Client[];
+  totalOwed: number;
 
-  constructor(private clientService: ClientService) { }
+  constructor(private clientService: ClientService) {}
 
   ngOnInit(): void {
-    this.clientService.getClients().subscribe(clients => {
+    this.clientService.getClients().subscribe((clients) => {
       this.clients = clients;
+      this.getTotalOwed();
     });
   }
 
+  getTotalOwed() {
+    this.totalOwed = this.clients.reduce((total, client) => {
+      return total + Number(client.balance);
+    }, 0);
+  }
 }
